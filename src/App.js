bde607ffde3826e026792a5dd11bbb53b61697ed
Bartosz S <skowyrab@gmail.com>
2024-04-24 22:02:52 +0200

    Create Grocery List

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from "react"
+import Header from './components/Header'
+import Products from './components/Products'
+import AddProduct from './components/AddProduct'
+
+import './App.css'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [products, setProducts] = useState([
+        {
+            "id": 1,
+            "name": "mleko",
+            "category": "diary",
+            "quantity": 1
+        }, {
+            "id": 2,
+            "name": "chleb",
+            "category": "bread",
+            "quantity": 1
+        }, {
+            "id": 3,
+            "name": "jabłka",
+            "category": "fruit&vagetables",
+            "quantity": 2
+        }])
+    const deleteProduct = (id) => { setProducts(products.filter((product) => product.id !== id))
+    }
+
+    const addProduct = (product) => {
+        const id = products.length + 1
+        const newProduct = { id, ...product }
+        setProducts([...products, newProduct])
+    }
+
+    return (
+        <div className="App">
+            <Header title="Lista zakupów"/>
+            <AddProduct onAdd={addProduct} />
+            {products.length > 0 ? <Products products={products} onDelete={deleteProduct} /> : <p>Brak produktów</p>}
+        </div>
+    )
 }
 
-export default App;
+export default App
